914d3fb2d2979185bf8bb23fbac54046e793452a
nelrsf <nel.rsf@gmail.com>
2020-10-08 09:37:40 -0500

    segundo avance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersListComponent } from './components/users-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users-list', component: UsersListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/User.ts
@@ -0,0 +1,9 @@
+export class User {
+    
+    id: Number;
+    name:string;
+    lastName:string;
+    telephone:string;
+    email:string;
+    address:string;
+}
